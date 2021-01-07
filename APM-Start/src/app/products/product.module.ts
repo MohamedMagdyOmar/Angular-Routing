4a3912e06940f95e68711821bc38164163de30d2
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2021-01-06 23:54:18 -0500

    Configuring Child Routes

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 import { RouterModule} from '@angular/router'
 import { SharedModule } from '../shared/shared.module';
 import {ProductResolver} from './product-resolver.service'
@@ -15,16 +17,35 @@ import {ProductResolver} from './product-resolver.service'
       { path: 'products/:id', component: ProductDetailComponent , 
        resolve:{
         resolvedData: ProductResolver
-      } },
-      { path: 'products/:id/edit', component: ProductEditComponent, resolve:{
-        resolvedData: ProductResolver
-      } }
+      } 
+    },
+      { path: 'products/:id/edit', component: ProductEditComponent, 
+        resolve:{ resolvedData: ProductResolver },
+        children: [
+          {
+            path:'',
+            redirectTo: 'info',
+            pathMatch: 'full'
+          },
+          {
+            path: 'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
+
+          }
+        ]
+    }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
