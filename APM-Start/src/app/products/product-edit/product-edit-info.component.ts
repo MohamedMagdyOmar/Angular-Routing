627498c279951b551f790c1874f5ca4f51b11f57
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2021-01-08 00:34:08 -0500

    Obtaining Data for Child Routes

--- APM-Start/src/app/products/product-edit/product-edit-info.component.ts
+++ APM-Start/src/app/products/product-edit/product-edit-info.component.ts
@@ -11,10 +11,19 @@ export class ProductEditInfoComponent implements OnInit {
   @ViewChild(NgForm, {static: false}) productForm: NgForm;
 
   errorMessage: string;
-  product = { id: 1, productName: 'test', productCode: 'test', description: 'test' };
+  //product = { id: 1, productName: 'test', productCode: 'test', description: 'test' };
+  product: Product;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.parent.data.subscribe(data => {
+      if(this.productForm)
+      {
+        // reset the form when the user selects toa dd new product. it reset the validation state of the form
+        this.productForm.reset()
+      }
+      this.product = data['resolvedData'].product;
+    })
   }
 }
